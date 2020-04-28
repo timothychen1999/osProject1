de30bbebde9057bb1916b73042028656ab0a10f4
ASUS-N58\owner <timothy950196@gmail.com>
2020-04-28 17:06:34 +0800

    minor edit

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+#include <unistd.h>
 #include <stdio.h>
+#define schcpu 2
+#define chdcpu 12
+void timeunit(){
+volatile unsigned long i; for(i=0;i<1000000UL;i++);
+}
+int pid[10];
+char name[10][50];
+int begin[10];
+int tu = 0;
+int 
+
+
